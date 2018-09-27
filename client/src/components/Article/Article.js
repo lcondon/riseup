198df3784afbbb62206c137677e20207ee35ca33
Lucas Condon <lcondon@bennington.edu>
2018-09-26 21:08:45 -0400

    papers

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core/styles";
-import Avatar from "@material-ui/core/Avatar";
-import PropTypes from "prop-types";
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({
   root: {
-    overflow: "hidden",
+    overflow: 'hidden',
     padding: `0 ${theme.spacing.unit * 3}px`
   },
   wrapper: {
-    maxWidth: 600,
-    marginLeft: "auto",
-    marginRight: "auto"
+    maxWidth: 1000,
+    marginLeft: 'auto',
+    marginRight: 'auto'
   },
   paper: {
-    margin: theme.spacing.unit,
-    padding: theme.spacing.unit * 2
+    margin: theme.spacing.unit * 2,
+    padding: theme.spacing.unit * 4
   }
 });
 
@@ -31,13 +31,14 @@ function Article(props) {
     <div className={classes.root}>
       <div className={classes.wrapper}>
         <Paper className={classes.paper}>
-        <h1 style={{ textAlign: 'center' }}>Article of the Day:</h1>
-        <h2 style={{ textAlign: 'center' }}>Article Title</h2>
-        <Divider/>
-        <p>Truncation should be conditionally applicable on this long line of text
-                    as this is a much longer line than what the container can support.</p>
-        
-
+          <h1 style={{ textAlign: 'center' }}>Article of the Day:</h1>
+          <h2 style={{ textAlign: 'center' }}>Article Title</h2>
+          <Divider />
+          <p>
+            Truncation should be conditionally applicable on this long line of
+            text as this is a much longer line than what the container can
+            support.
+          </p>
         </Paper>
         <Paper className={classes.paper}>
           <Grid alignItems="center" container wrap="nowrap" spacing={16}>
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -2,15 +2,23 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    overflow: 'hidden',
+    padding: `0 ${theme.spacing.unit * 3}px`
+  },
+  wrapper: {
+    maxWidth: 1000,
+    marginLeft: 'auto',
+    marginRight: 'auto'
   },
   paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary
+    margin: theme.spacing.unit * 2,
+    padding: theme.spacing.unit * 4
   }
 });
 
@@ -26,67 +34,84 @@ const signStyles = {
   backgroundColor: '#44C2CE'
 };
 
-const Landing = props => {
-  const { classes } = props;
-  return (
-    <div>
-      <Grid container direction="row" justify="center" spacing={24}>
-        <Grid container justify="center">
-          <Grid item>
-            <img
-              className="responsive-img"
-              justify="center"
-              src="../../images/riseUp.png"
-            />
+class Landing extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Grid container direction="row" justify="center" spacing={24}>
+          <Grid container justify="center">
+            <Grid item>
+              <img
+                className="responsive-img"
+                justify="center"
+                src="../../images/riseUp.png"
+              />
+            </Grid>
           </Grid>
+          <div className={classes.root}>
+            <div className={classes.wrapper}>
+              <Paper className={classes.paper}>
+                <Grid container justify="center">
+                  <Grid item xs={10}>
+                    <h1 style={{ textAlign: 'center' }}>riseUP</h1>
+                    <Divider />
+                  </Grid>
+                  <Grid item xs={10}>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Ut libero metus, posuere quis odio sed, molestie ornare
+                      urna. Curabitur pulvinar vulputate augue ac blandit. Nulla
+                      dictum eu neque ac consectetur. Sed ut nisl ut lacus
+                      tincidunt bibendum eu ut velit. Donec eu leo ligula.
+                      Nullam posuere lobortis laoreet. Morbi maximus ultricies
+                      lorem, quis maximus ligula tincidunt in. Pellentesque
+                      habitant morbi tristique senectus et netus et malesuada
+                      fames ac turpis egestas. Maecenas condimentum, lacus nec
+                      egestas scelerisque, massa ligula ultrices mi, ac
+                      ullamcorper ante nisi at leo. Integer condimentum metus
+                      odio, vitae maximus nulla lobortis a. Quisque iaculis et
+                      nunc id pretium. Curabitur facilisis iaculis dapibus.
+                      Pellentesque dignissim velit ac lectus euismod, eu
+                      placerat est ultrices. Etiam ut mi feugiat, cursus.
+                    </p>
+                  </Grid>
+                  <Grid container justify="center" spacing={24}>
+                    <Grid item xs={10} sm={5}>
+                      <a style={{ textDecoration: 'none' }} href="/login">
+                        <Button
+                          id="landingLogBtn"
+                          style={logStyles}
+                          variant="contained"
+                          color="primary">
+                          Login
+                        </Button>
+                      </a>
+                    </Grid>
+                    <Grid item xs={10} sm={5}>
+                      <a style={{ textDecoration: 'none' }} href="/signup">
+                        <Button
+                          id="landingSignBtn"
+                          style={signStyles}
+                          variant="contained"
+                          color="secondary">
+                          Sign Up
+                        </Button>
+                      </a>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </div>
+          </div>
         </Grid>
-        <Grid item xs={10} sm={8}>
-          <h1 style={{ textAlign: 'center' }}>riseUP</h1>
-        </Grid>
-        <Grid item xs={10} sm={8}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut libero
-            metus, posuere quis odio sed, molestie ornare urna. Curabitur
-            pulvinar vulputate augue ac blandit. Nulla dictum eu neque ac
-            consectetur. Sed ut nisl ut lacus tincidunt bibendum eu ut velit.
-            Donec eu leo ligula. Nullam posuere lobortis laoreet. Morbi maximus
-            ultricies lorem, quis maximus ligula tincidunt in. Pellentesque
-            habitant morbi tristique senectus et netus et malesuada fames ac
-            turpis egestas. Maecenas condimentum, lacus nec egestas scelerisque,
-            massa ligula ultrices mi, ac ullamcorper ante nisi at leo. Integer
-            condimentum metus odio, vitae maximus nulla lobortis a. Quisque
-            iaculis et nunc id pretium. Curabitur facilisis iaculis dapibus.
-            Pellentesque dignissim velit ac lectus euismod, eu placerat est
-            ultrices. Etiam ut mi feugiat, cursus.
-          </p>
-        </Grid>
-        <Grid container justify="center" spacing={24}>
-          <Grid item xs={10} sm={4}>
-            <a style={{ textDecoration: 'none' }} href="/login">
-              <Button
-                id="landingLogBtn"
-                style={logStyles}
-                variant="contained"
-                color="primary">
-                Login
-              </Button>
-            </a>
-          </Grid>
-          <Grid item xs={10} sm={4}>
-            <a style={{ textDecoration: 'none' }} href="/signup">
-              <Button
-                id="landingSignBtn"
-                style={signStyles}
-                variant="contained"
-                color="secondary">
-                Sign Up
-              </Button>
-            </a>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default Landing;
+export default withStyles(styles)(Landing);
