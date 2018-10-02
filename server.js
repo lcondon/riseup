f959b3f27080f1e08595fdea03e07493db4b3c34
Lucas Condon <lcondon@bennington.edu>
2018-10-02 12:25:12 -0400

    sox

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const dbConnection = require('./db');
 const db = require('./models'); // loads our connection to the mongo database
 // const passport = require("./passport");
 const path = require('path');
+const socket = require('socket.io');
+const http = require('https');
 // const cookieparser = require("cookie-parser");
 // const flash = require("connect-flash");
 // const logger = require("morgan");
@@ -15,6 +17,26 @@ const PORT = process.env.PORT || 3001;
 const userRouter = require('./routes/index');
 
 const app = express();
+
+const server = http.Server(app);
+const io = socket(server);
+
+io.on('connection', function(socket) {
+  socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function(data) {
+  //   console.log(data);
+  // });
+});
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  //  res.io = skt;
+  next();
+});
 
 // app.use(logger("dev"));
 app.use(bodyParser.json());
@@ -46,21 +68,26 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(userRouter);
 app.get('/api/test', (req, res) => res.json({}));
 
-app.listen(PORT, function() {
+// const http = require('http').createServer(app);
+
+// var fs = require('fs');
+server.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-const http = require('http');
-const socketIO = require('socket.io');
-const server = http.createServer(app);
-const io = socketIO(server);
+// const server = http.createServer(app);
 // const port2 = 4001
-io.on('connection', socket => {
-  console.log('User connected');
+// io.on('connection', socket => {
+//   console.log('User connected');
+//   socket.on('chat message', function(msg) {
+//     io.emit('chat message', msg);
+//   });
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+// http.listen(80, function() {
+//   console.log('listening on *:' + 80);
+// });
 
 // server.listen(port2, () => console.log(`Listening on port ${port2}`))
