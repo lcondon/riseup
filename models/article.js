c544386ec7da86627131f396cd15158dc7c42678
gibbsk12 <kate.virginia.gibbs@gmail.com>
2018-10-03 21:20:30 -0400

    article model

---
+++ models/article.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  date: { type: String,  required: true},
+  text: { type: String, required: true}
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
