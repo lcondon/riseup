06454ea43ec9d3faf40701b7f54ac08ff2d4eae0
gibbsk12 <kate.virginia.gibbs@gmail.com>
2018-10-03 20:22:53 -0400

    socket.io returning messages

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import SideBar from '../SideBar';
-import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Hidden from '@material-ui/core/Hidden';
-import withWidth from '@material-ui/core/withWidth';
-import compose from 'recompose/compose';
-import Divider from '@material-ui/core/Divider';
-import io from 'socket.io-client';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import SideBar from "../SideBar";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import withWidth from "@material-ui/core/withWidth";
+import compose from "recompose/compose";
+import Divider from "@material-ui/core/Divider";
+import io from "socket.io-client";
 
-var Socket = window['MozWebSocket'] || window['WebSocket'];
+var Socket = window["MozWebSocket"] || window["WebSocket"];
 
-if (Socket) {
-  let socket = io.connect('http://localhost:3001');
-  // var ws = new Socket('ws://localhost:3001/');
-  socket.onopen = () => {
-    console.log('onopen');
-  };
-  socket.emit('news', { hello: 'world' });
-  // ws.onerror = function() { console.log('onerror attached to websocket object.'); };
-  // ws.onclose = function() { console.log('onclose'); };
-  socket.onmessage = function(evt) {
-    console.log('onmessage: ' + evt.data);
-  };
-}
+// if (Socket) {
+//   let socket = io.connect("http://localhost:3001");
+//   // var ws = new Socket('ws://localhost:3001/');
+//   socket.onopen = () => {
+//     console.log("onopen");
+//   };
+//   socket.emit("news", { hello: "world" });
+//   // ws.onerror = function() { console.log('onerror attached to websocket object.'); };
+//   // ws.onclose = function() { console.log('onclose'); };
+//   socket.onmessage = function(evt) {
+//     console.log("onmessage: " + evt.data);
+//   };
+// }
 
 const styles = theme => ({
   root: {
-    marginTop: '10px',
-    overflow: 'hidden',
+    marginTop: "10px",
+    overflow: "hidden",
     padding: `0 ${theme.spacing.unit * 3}px`
   },
   wrapper: {
     maxWidth: 1000,
-    marginLeft: 'auto',
-    marginRight: 'auto'
+    marginLeft: "auto",
+    marginRight: "auto"
   },
   paper: {
     margin: theme.spacing.unit * 2,
@@ -49,31 +49,55 @@ const styles = theme => ({
   textField: {
     marginLeft: theme.spacing.unit * 2,
     marginRight: theme.spacing.unit * 2,
-    width: '100%'
+    width: "100%"
   },
   title: {
-    'font-family': 'Rubik',
-    color: '#01163D'
+    "font-family": "Rubik",
+    color: "#01163D"
   },
   subtitle: {
-    'font-family': 'Rubik',
-    color: '#389EA8'
+    "font-family": "Rubik",
+    color: "#389EA8"
   }
 });
 
 class Messages extends React.Component {
-  state = {
-    messages: ['hello world']
-  };
-  // constructor() {
-  //   super();
 
-  //   // this.state = {
-  //   //   endpoint: "http://127.0.0.1:4001"
-  //   // }
+  
 
-  //   // this.socket = window.io();
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+      message: "",
+      pastMessages: []
+    };
+
+    this.socket = io("http://localhost:3001");
+
+    this.socket.on("RECEIVE_MESSAGE", function(data) {
+      // console.log(data)
+      addMessage(data);
+    });
+
+    const addMessage = data => {
+      this.setState({ pastMessages: [...this.state.pastMessages, data] });
+      console.log(this.state.pastMessages);
+    };
+
+    this.sendMessage = ev => {
+      ev.preventDefault();
+      this.socket.emit("SEND_MESSAGE", {
+        user: this.props.user.firstName,
+        message: this.state.message,
+      });
+      this.setState({ message: "" });
+    };
+    this.componentDidMount = () => {
+      this.setState({ user: this.props.user });
+    }
+  }
 
   // componentDidMount() {
   //   socket.on('news', function(data) {
@@ -82,9 +106,9 @@ class Messages extends React.Component {
   //   });
   // }
 
-  send = () => {
-    this.socket.emit('change color', 'red');
-  };
+  // send = () => {
+  //   this.socket.emit('change color', 'red');
+  // };
 
   render() {
     const { classes } = this.props;
@@ -97,11 +121,11 @@ class Messages extends React.Component {
       <div className={classes.root}>
         <div className={classes.wrapper}>
           <Paper className={classes.paper}>
-            <h1 style={{ textAlign: 'center' }} className={classes.title}>
+            <h1 style={{ textAlign: "center" }} className={classes.title}>
               Messages
             </h1>
             <Divider />
-            <h2 style={{ textAlign: 'center' }} className={classes.subtitle}>
+            <h2 style={{ textAlign: "center" }} className={classes.subtitle}>
               Maybe a quote to discuss
             </h2>
             <Grid container justify="center">
@@ -109,7 +133,8 @@ class Messages extends React.Component {
                 className={classes.button}
                 id="submitCommentBtn"
                 variant="contained"
-                color="secondary">
+                color="secondary"
+              >
                 Match Me
               </Button>
             </Grid>
@@ -128,16 +153,26 @@ class Messages extends React.Component {
                   container
                   alignItems="stretch"
                   direction="column"
-                  justify="flex-end">
+                  justify="flex-end"
+                >
                   <Grid item>
-                    <div className="messages">{this.state.messages}</div>
+                    <div className="messages">
+                      {this.state.pastMessages.map(message => {
+                        return (
+                          <div>
+                           <strong> {message.user} </strong>: {message.message}
+                          </div>
+                        );
+                      })}
+                    </div>
                   </Grid>
                   <Grid
                     container
                     spacing={24}
                     direction="row"
                     alignItems="center"
-                    justify="space-between">
+                    justify="space-between"
+                  >
                     <Grid item xs={10}>
                       <TextField
                         id="standard-multiline-flexible"
@@ -146,9 +181,11 @@ class Messages extends React.Component {
                         style={{ margin: 10 }}
                         fullWidth
                         margin="normal"
+                        value={this.state.message}
+                        onChange={ev =>
+                          this.setState({ message: ev.target.value })
+                        }
                         label="Message"
-                        // value = {this.state.message}
-                        // onChange = {ev=> this.setState({message: ev.target.value})}
                         className={classes.textField}
                         InputLabelProps={{
                           shrink: true
@@ -162,7 +199,8 @@ class Messages extends React.Component {
                         variant="outlined"
                         size="medium"
                         color="secondary"
-                        onClick={() => this.send()}>
+                        onClick={ev => this.sendMessage(ev)}
+                      >
                         Send
                       </Button>
                     </Grid>
