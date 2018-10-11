34b3648c07325fec3ee77e6a0436c68e01264a31
Lucas Condon <lcondon@bennington.edu>
2018-10-11 11:40:15 -0400

    node env

--- server.js
+++ server.js
@@ -37,16 +37,17 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', function(req, res) {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
+} else if (process.env.NODE_ENV === 'production') {
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
 }
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 app.use(
   session({
